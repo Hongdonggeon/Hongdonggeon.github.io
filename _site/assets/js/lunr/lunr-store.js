7219cfa8e4c39cb0f23fda08a75984590eae0ffa
Hongdonggeon <ghd700@naver.com>
2022-02-15 22:05:47 +0900

    junit

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -5,33 +5,33 @@ var store = [{
         "url": "/java/JDK,JRE,JVM/",
         "teaser": null
       },{
-        "title": "2022.02.09 TIL",
-        "excerpt":"Junit     assertThatExceptionOfType   assertThatIoException   @DisplayName   @ParameterizedTest   @CsvSource  ","categories": ["til"],
+        "title": "2022.02.09 Junit",
+        "excerpt":"우테코 첫 미션으로 TDD 기법을 사용해 자동차 경주 게임을 구현한다. 이전에 프로덕션 코드를 작성하고 간단한 단위 테스트 코드를 작성한 적은 있지만 TDD는 처음이다. 지금까지는 Junit은 테스트 코드를 작성하게 지원해주는 라이브러리 정도라고만 알고 있었는데, Junit에 대해 궁금증이 생겨 정리해보았다. Junit5 Junit5는 이전 버전과 다르게 세개의 서브 프로젝트로 이루어져 있다. Junit5은 Junit...","categories": ["til"],
         "tags": [],
-        "url": "/til/2022.02.09-TIL/",
+        "url": "/til/Junit/",
         "teaser": null
       },{
-        "title": "2022.02.10 TIL",
+        "title": "2022.02.10 정규표현식, 팩토리 메서드 패턴, 유틸 클래스",
         "excerpt":"정규 표현식   팩토리 메서드 패턴   유틸 클래스  ","categories": ["til","java"],
         "tags": [],
-        "url": "/til/java/2022.02.10-TIL/",
+        "url": "/til/java/%EC%A0%95%EA%B7%9C-%ED%91%9C%ED%98%84%EC%8B%9D,-%ED%8C%A9%ED%86%A0%EB%A6%AC-%EB%A9%94%EC%84%9C%EB%93%9C-%ED%8C%A8%ED%84%B4,-%EC%9C%A0%ED%8B%B8-%ED%81%B4%EB%9E%98%EC%8A%A4/",
         "teaser": null
       },{
-        "title": "2022.02.11 TIL",
-        "excerpt":"스트림 Colloctors,joining()  상대방 커밋 이력과 코드 가져오기  ","categories": ["til","java"],
+        "title": "2022.02.11 스트림 joining, 상대방 커밋 이력 가져오기",
+        "excerpt":"스트림 Colloctors.joining()  상대방 커밋 이력과 코드 가져오기  ","categories": ["til","java"],
         "tags": [],
-        "url": "/til/java/2022.02.11-TIL/",
-        "teaser": null
-      },{
-        "title": "2022.02.13 TIL",
-        "excerpt":"함수 vs 메서드 모던 자바 인 액션을 읽는 중, 책에서 설명을 할 때 메서드와 함수를 다른 의미로 사용하는 듯 했다. 함수와 메서드는 같은 의미지 않나라는 생각이 들었고, java 공부를 시작하면서 함수라는 단어는 사용을 잘 안 한듯한 느낌이 들었다. 함수와 메서드의 용어에 대해서 생각해보게 되었다. 함수 프로그래밍 언어에 있어서 함수라는 것은...","categories": ["java","til"],
-        "tags": [],
-        "url": "/java/til/2022.02.13-TIL/",
+        "url": "/til/java/%EC%8A%A4%ED%8A%B8%EB%A6%BC-joining,-%EC%83%81%EB%8C%80%EB%B0%A9-%EC%BB%A4%EB%B0%8B-%EC%9D%B4%EB%A0%A5-%EA%B0%80%EC%A0%B8%EC%98%A4%EA%B8%B0/",
         "teaser": null
       },{
         "title": "Java 8에서는 무슨일이 일어났는가?",
         "excerpt":"자바 8, 9, 10, 11 무슨일이 일어나고 있는가 자바 8은 간결한 코드, 멀티코어 프로세서의 쉬운 활용이라는 두 가지 요구사항을 기반으로 한다. 자바 8이 등장하기 이전의 자바 프로그램은 코어 중 하나만을 사용했다. 그래서 나머지 코어를 활용하려면 스레드를 사용하는 것이 좋다는 말이 있었다. 하지만 스레드를 사용하면 관리하기 어렵고 많은 문제가 발생할 수...","categories": ["java"],
         "tags": [],
         "url": "/java/%EC%9E%90%EB%B0%94-8%EC%97%90%EC%84%9C%EB%8A%94-%EB%AC%B4%EC%8A%A8%EC%9D%BC%EC%9D%B4-%EC%9D%BC%EC%96%B4%EB%82%AC%EB%8A%94%EA%B0%80/",
+        "teaser": null
+      },{
+        "title": "2022.02.13 함수 vs 메서드 , TDA 법칙, 커밋에는 어떤 내용이",
+        "excerpt":"함수 vs 메서드 모던 자바 인 액션을 읽는 중, 책에서 설명을 할 때 메서드와 함수를 다른 의미로 사용하는 듯 했다. 함수와 메서드는 같은 의미지 않나라는 생각이 들었고, java 공부를 시작하면서 함수라는 단어는 사용을 잘 안 한듯한 느낌이 들었다. 함수와 메서드의 용어에 대해서 생각해보게 되었다. 함수 프로그래밍 언어에 있어서 함수라는 것은...","categories": ["java","til"],
+        "tags": [],
+        "url": "/java/til/%ED%95%A8%EC%88%98-vs-%EB%A9%94%EC%84%9C%EB%93%9C/",
         "teaser": null
       }]
